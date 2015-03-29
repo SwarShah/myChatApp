c4244f366b037c9102be454736c41e4e0cd7c0b6
SwarShah <swar12894@gmail.com>
2015-03-29 16:18:13 -0400

    ChatActivity

--- gen/com/example/mychatapp/R.java
+++ gen/com/example/mychatapp/R.java
@@ -36,10 +36,12 @@ public final class R {
         public static final int nameTV=0x7f090001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_chat=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int chat=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
@@ -48,6 +50,7 @@ public final class R {
         public static final int enterName=0x7f060003;
         public static final int hello_world=0x7f060001;
         public static final int name=0x7f060005;
+        public static final int title_activity_chat=0x7f060006;
     }
     public static final class style {
         /** 
