c6b8f74830a506840c6b9757f341db04d47ec5c4
SwarShah <swar12894@gmail.com>
2015-03-29 16:11:27 -0400

    On connect btn event

--- src/com/example/mychatapp/MainActivity.java
+++ src/com/example/mychatapp/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.mychatapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -20,7 +19,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         btnConnect = (Button)findViewById(R.id.connectBTN);
         etName = (EditText)findViewById(R.id.nameET);
-        
+
+        btnConnect.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				name = etName.getText().toString();
+				if(!name.trim().isEmpty()){
+					Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG);
+				}
+				else{
+					Toast.makeText(getApplicationContext(), "Please enter your name.", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
     }
 
 }
