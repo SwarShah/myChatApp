6809176ac8ae74902cf6da3f6f9e7444b827829a
SwarShah <swar12894@gmail.com>
2015-03-29 16:08:21 -0400

    Change in declaration

--- src/com/example/mychatapp/MainActivity.java
+++ src/com/example/mychatapp/MainActivity.java
@@ -4,18 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-	
+	private Button btnConnect;
+	private EditText etName;
+	String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnConnect = (Button)findViewById(R.id.connectBTN);
-        EditText etName = (EditText)findViewById(R.id.nameET);
+        btnConnect = (Button)findViewById(R.id.connectBTN);
+        etName = (EditText)findViewById(R.id.nameET);
         
     }
 
