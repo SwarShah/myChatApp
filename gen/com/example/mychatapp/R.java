fd3fdb182200ba93ea6501329589893fb9f549e8
SwarShah <swar12894@gmail.com>
2015-03-29 15:55:43 -0400

    Initial Design

--- gen/com/example/mychatapp/R.java
+++ gen/com/example/mychatapp/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int orange=0x7f040000;
+        public static final int white=0x7f040001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -29,7 +30,10 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090000;
-        public static final int action_settings=0x7f090001;
+        public static final int action_settings=0x7f090004;
+        public static final int connectBTN=0x7f090003;
+        public static final int nameET=0x7f090002;
+        public static final int nameTV=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,6 +44,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int connect=0x7f060004;
+        public static final int enterName=0x7f060003;
         public static final int hello_world=0x7f060001;
     }
     public static final class style {
