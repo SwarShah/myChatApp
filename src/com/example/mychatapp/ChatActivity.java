25e7736ef04a0a7033fd5610c940cbd6a7bc4000
SwarShah <swar12894@gmail.com>
2015-04-11 20:48:50 -0400

    intent getextra

--- src/com/example/mychatapp/ChatActivity.java
+++ src/com/example/mychatapp/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.mychatapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ChatActivity extends Activity {
@@ -9,5 +10,9 @@ public class ChatActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chat);
+		//Person name from main activity
+		Intent intent = getIntent();
+		intent.getStringExtra("Name");
+		
 	}
 }
