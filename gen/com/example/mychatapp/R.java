08496e25b53f47408222b4dee6559f60b6d10f85
SwarShah <swar12894@gmail.com>
2015-04-11 20:34:46 -0400

    chat activity design

--- gen/com/example/mychatapp/R.java
+++ gen/com/example/mychatapp/R.java
@@ -29,11 +29,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f090000;
-        public static final int action_settings=0x7f090004;
-        public static final int connectBTN=0x7f090003;
-        public static final int nameET=0x7f090002;
-        public static final int nameTV=0x7f090001;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int RelativeLayout1=0x7f090005;
+        public static final int action_settings=0x7f090009;
+        public static final int connectBTN=0x7f090008;
+        public static final int msgBtn=0x7f090002;
+        public static final int msgInput=0x7f090003;
+        public static final int msgListView=0x7f090001;
+        public static final int nameET=0x7f090007;
+        public static final int nameTV=0x7f090006;
+        public static final int sendBtn=0x7f090004;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
