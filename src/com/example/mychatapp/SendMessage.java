e71a94799c11da50ab10d7994e1c74cfb6b12b5c
SwarShah <swar12894@gmail.com>
2015-04-13 01:53:21 -0400

    SendMessage comments

--- src/com/example/mychatapp/SendMessage.java
+++ src/com/example/mychatapp/SendMessage.java
@@ -2,11 +2,13 @@ package com.example.mychatapp;
 
 import javax.json.Json;
 
+//Class for contents of Message that needs to send message
 public class SendMessage {
 	private String flag;
 	private String sessionId;
 	private String message;
 	
+	//Constructor
 	public SendMessage(String flag, String sessionId, String message) {
 		super();
 		this.flag = flag;
@@ -14,6 +16,7 @@ public class SendMessage {
 		this.message = message;
 	}
 	
+	//toJSON method to convert the data in JSON object
 	public String toJSON(){
 		return Json.createObjectBuilder()
 				.add("flag", flag)
