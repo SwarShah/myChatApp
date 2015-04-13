a7cb3f6f53b1bdc8c1372ebdff2918e20b3511c0
SwarShah <swar12894@gmail.com>
2015-04-13 00:32:56 -0400

    added self to check for msg from self

--- src/com/example/mychatapp/ReceiveMessage.java
+++ src/com/example/mychatapp/ReceiveMessage.java
@@ -3,11 +3,12 @@ package com.example.mychatapp;
 public class ReceiveMessage {
 	private String from;
 	private String message;
-	
-	public ReceiveMessage(String from, String message) {
+	private Boolean self;
+	public ReceiveMessage(String from, String message, Boolean self) {
 		super();
 		this.setFrom(from);
 		this.setMessage(message);
+		this.setSelf(self);
 	}
 
 	public String getFrom() {
@@ -24,6 +25,14 @@ public class ReceiveMessage {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public Boolean getSelf() {
+		return self;
+	}
+
+	public void setSelf(Boolean self) {
+		this.self = self;
 	}
 	
 }
