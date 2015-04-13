5abd22b0c976f642b8a2463f7cd223e6abd41c57
SwarShah <swar12894@gmail.com>
2015-04-13 01:59:32 -0400

    MainActivity comments

--- src/com/example/mychatapp/MainActivity.java
+++ src/com/example/mychatapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	//Declaration
 	private Button btnConnect;
 	private EditText etName;
 	String name;
@@ -20,21 +21,25 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         btnConnect = (Button)findViewById(R.id.connectBTN);
         etName = (EditText)findViewById(R.id.nameET);
-
+        //On press of connect button
         btnConnect.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				//Getting text of textbox
 				name = etName.getText().toString();
+				//if it's not empty
 				if(!name.trim().isEmpty()){
 					//Testing
 					//Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+					//Switch to next screen
 					Intent i = new Intent(getApplicationContext(), ChatActivity.class);
 					i.putExtra("Name", name);
 					startActivity(i);
 				}
 				else{
+					//Show error message on invalid
 					Toast.makeText(getApplicationContext(), "Please enter your name.", Toast.LENGTH_LONG).show();
 				}
 			}
