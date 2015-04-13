6805d1cba7eda1c5e56f7c175208fdad25e21be8
SwarShah <swar12894@gmail.com>
2015-04-13 00:38:42 -0400

    adding all files

--- gen/com/example/mychatapp/R.java
+++ gen/com/example/mychatapp/R.java
@@ -11,7 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int green=0x7f040002;
+        public static final int grey=0x7f040003;
         public static final int orange=0x7f040000;
+        public static final int red=0x7f040004;
         public static final int white=0x7f040001;
     }
     public static final class dimen {
@@ -26,25 +29,30 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int from=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int you=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
-        public static final int RelativeLayout1=0x7f090007;
-        public static final int action_settings=0x7f09000b;
-        public static final int connectBTN=0x7f09000a;
-        public static final int leaveBtn=0x7f090001;
-        public static final int msgBtn=0x7f090004;
-        public static final int msgInput=0x7f090005;
-        public static final int msgListView=0x7f090003;
-        public static final int nameET=0x7f090009;
-        public static final int nameTV=0x7f090008;
-        public static final int onlineTv=0x7f090002;
-        public static final int sendBtn=0x7f090006;
+        public static final int RelativeLayout1=0x7f090006;
+        public static final int action_settings=0x7f09000c;
+        public static final int connectBTN=0x7f090009;
+        public static final int lblMsgFrom=0x7f09000a;
+        public static final int msgBtn=0x7f090003;
+        public static final int msgInput=0x7f090004;
+        public static final int msgListView=0x7f090002;
+        public static final int nameET=0x7f090008;
+        public static final int nameTV=0x7f090007;
+        public static final int onlineTv=0x7f090001;
+        public static final int sendBtn=0x7f090005;
+        public static final int txtMsg=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int left=0x7f030002;
+        public static final int right=0x7f030003;
     }
     public static final class menu {
         public static final int chat=0x7f080000;
