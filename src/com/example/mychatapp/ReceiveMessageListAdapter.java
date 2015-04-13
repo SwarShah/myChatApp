99839939212381b4a0ad3b86b966359ad439d0c5
SwarShah <swar12894@gmail.com>
2015-04-13 00:35:32 -0400

    adapter to set data and add view accordingly

--- src/com/example/mychatapp/ReceiveMessageListAdapter.java
+++ src/com/example/mychatapp/ReceiveMessageListAdapter.java
@@ -1,11 +1,16 @@
 package com.example.mychatapp;
 
+
 import java.util.List;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class ReceiveMessageListAdapter extends BaseAdapter {
 	
@@ -20,25 +25,37 @@ public class ReceiveMessageListAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return messagesItems.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		return null;
+		return messagesItems.get(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
-
+	@SuppressLint("InflateParams")
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		ReceiveMessage rm = messagesItems.get(position);
+		LayoutInflater mInflater = (LayoutInflater) context
+				.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		if (messagesItems.get(position).getSelf()) {
+			convertView = mInflater.inflate(R.layout.right,null);
+		} else {
+			convertView = mInflater.inflate(R.layout.left,null);
+		}
+		TextView fromLbl = (TextView) convertView.findViewById(R.id.lblMsgFrom);
+		TextView msgLbl = (TextView) convertView.findViewById(R.id.txtMsg);
+		msgLbl.setText(rm.getMessage());
+		fromLbl.setText(rm.getFrom());
+		return convertView;
 	}
 
 }
